f711e52766e98515da285cb66685352653ccdb4e
懋和道人 <15062733214@qq.com>
2026-02-15 08:36:47 +0800

    Fix URL format in ACCESS_WHITELIST

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ ACCESS_WHITELIST = [
     "https://www.gymxbl.com/",
     "https://www.quji.org/",
     "https://www.52txr.cn/",
-    "https://www.dalao.net"
+    "https://www.dalao.net/"
     # 可继续添加需要跳过访问检测的链接
 ]
 
